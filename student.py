6fb7cd838e3ed977cdfac38171e90d3aeb6cb3c1
ZainabYaqubi <zainabyaqubi456@gmail.com>
2026-07-08 10:16:38 +0430

    courses is a list

--- student.py
+++ student.py
@@ -3,7 +3,7 @@ class student:
         self.__student_id = student_id
         self.__name = name
         self.email = email
-        self.courses = courses
+        self.courses =[]
 
     def get_id(self):
         return self.__student_id
