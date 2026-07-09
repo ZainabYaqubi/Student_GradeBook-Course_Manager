f35f033f786fc991fa4c71329f610201a134414f
ZainabYaqubi <zainabyaqubi456@gmail.com>
2026-07-09 20:09:13 +0430

    Add quiz class inherited from assessment class

---
+++ quiz.py
@@ -0,0 +1,16 @@
+from assessment import Assessment
+
+class Quiz(Assessment):
+    def __init__(self, title, max_score):
+        super().__init__(title, max_score)
+
+    def grade_massage(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 70:
+            return f"Great quiz result: {percentage:.1f}%"
+        else:
+            return f"Need more practice: {percentage:.1f}%"
+
+    def display_info(self):
+        print(f"Quiz 1: {self.title}")
+        print(f"Max Score: {self.max_score}")
