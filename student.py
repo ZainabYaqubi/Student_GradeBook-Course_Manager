a4d2ce0ca2242f52fb4a0b8043d78223adbe87e6
ZainabYaqubi <zainabyaqubi456@gmail.com>
2026-07-20 10:21:04 +0430

    .

--- student.py
+++ student.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self, student_id, name, email, courses):
+    def __init__(self, student_id, name, email):
         self.__student_id = student_id
         self.__name = name
         self.email = email
