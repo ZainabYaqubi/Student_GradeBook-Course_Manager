a54d631f77cbe106da784ad0581ce7cd06beee9b
ZainabYaqubi <zainabyaqubi456@gmail.com>
2026-07-20 10:15:39 +0430

    Update grade book class

--- grade_book.py
+++ grade_book.py
@@ -83,3 +83,21 @@ class GradeBook:
                 print(f"{assessment}: {score}")
 
         print(f"\nAverage: {self.calculate_average(student_id, course_code):.2f}")
+
+    def letter_grades(self, average):
+        if average >= 90:
+            return "A"
+
+        elif average >= 80:
+            return "B"
+
+        elif average >= 70:
+            return "C"
+
+        elif average >= 60:
+            return "D"
+
+        else:
+            return "F"
+
+    
